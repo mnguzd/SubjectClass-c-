d4a9bd54e4d43f438d426c7d8b332fa6df1e131f
mnguzd <roman.kioroglo@gmail.com>
2020-02-08 00:13:40 +0200

    Merge release

--- Subject.cs
+++ Subject.cs
@@ -5,15 +5,12 @@ using System.Text;
 
 namespace LabaN7
 {
-    class Subject : InterfaceAll
+    class Subject 
     {
         public string Name { get; set; }
         public ushort Year { get; set; }
         public string Faculty { get; set; }
         public string Department { get; set; }
         public ushort Hours { get; set; }
-       
-
-
     };
 }
